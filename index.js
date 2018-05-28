f62e66702806af08d30b0a7dc7bf6dfca34d4853
Ricardo Tribaldos <579671+swordf1zh@users.noreply.github.com>
2018-05-28 00:26:42 -0500

    Add express router wrapper module

---
+++ index.js
@@ -0,0 +1,26 @@
+const mpdRouter = require('express').Router();
+const debug = require('debug')('mattermoster:mpd-plugin');
+
+/**
+ * POST /mpd
+ */
+
+mpdRouter.post('/', (req, res) => {
+  const mmMpd = require('./mm-mpd');
+  mmMpd.do(req.body).then(
+    (result) => success(res, result),
+    (err) => error(res, err)
+  );
+});
+
+function success(response, result) {
+  debug(result);
+  response.json(result);
+}
+
+function error(response, error) {
+  debug(error);
+  response.status(500).json({ msg: 'Something broke!'});
+}
+
+module.exports = mpdRouter;
